3661dbd4dcb87ef2dd752cfd40b8e00e8e2acb4c
Pedro <p3dr0sk0sk7160@gmail.com>
2025-10-14 21:50:43 -0300

    primeiro commit

---
+++ logica-js3/lista01/q01.js
@@ -0,0 +1,18 @@
+// Lista 01 - Lógica de programação: praticando com desafios - Alura
+
+// 1. Crie um programa que verifica se uma palavra ou frase é um palíndromo.
+
+verificarPalindromo ();
+
+function verificarPalindromo () {
+    let palavra = prompt ('Digite uma palavra ou frase:');
+    let separarPalavra = palavra.split ('');
+    let inverterPalavra = separarPalavra.reverse ('');
+    inverterPalavra = inverterPalavra.join ('');
+    
+    if (palavra == inverterPalavra) {
+        console.log ('Isto é um palíndromo.');
+    } else {
+        console.log ('Isto não é um palíndromo.');
+    }
+}
---
+++ logica-js3/lista01/q02.js
@@ -0,0 +1,29 @@
+// Lista 01 - Lógica de programação: praticando com desafios - Alura
+
+// 2. Crie um programa com uma função que receba três números como argumentos 
+// e os retorne em ordem crescente. Exiba os números ordenados.
+
+let num1 = Number (prompt ('Digite um número:'));
+
+while (isNaN (num1)) {
+    num1 = Number (prompt ('Digite um número:'));
+}
+
+let num2 = Number (prompt ('Digite outro número:'));
+
+while (isNaN (num2)) {
+    num1 = Number (prompt ('Digite outro número:'));
+}
+
+let num3 = Number (prompt ('Digite um último número:'));
+
+while (isNaN (num3)) {
+    num1 = Number (prompt ('Digite um último número:'));
+}
+
+ordenarNumeros ();
+
+function ordenarNumeros (num1, num2, num3) {
+    const numerosOrdenados = [num1, num2, num3].sort ((x, y) => x - y);
+    console.log (`Números ordenados: ${numerosOrdenados.join (', ')}`);
+}
