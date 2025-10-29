91282f47d259de0c03387be90a6be7aa923ebc7b
Pedro <p3dr0sk0sk7160@gmail.com>
2025-10-29 15:01:42 -0300

    ajustes lista03

--- logica-js3/lista03/q03.js
+++ logica-js3/lista03/q03.js
@@ -3,3 +3,18 @@
 // 3. Implemente uma função que aceite um número como parâmetro e utilize 
 // um bloco condicional para determinar se o número é par ou ímpar.
 
+let numero = prompt ('Digite um número:');
+
+verificarParImpar (numero);
+
+function verificarParImpar (num) {
+
+    num = Number (num);
+
+    if (num % 2 == 0) {
+        alert ('Este é um número par.');
+    } else {
+        alert ('Este é um número ímpar.');
+    }
+
+} 
--- logica-js3/lista03/q04.js
+++ logica-js3/lista03/q04.js
@@ -1,1 +1,38 @@
 // Lista 03 - Lógica de programação: praticando com desafios - Alura
+
+// 4. Crie uma função que converta a temperatura de Celsius para Fahrenheit 
+// e vice-versa. Utilize uma variável para indicar a escala desejada e um 
+// bloco condicional para determinar qual conversão realizar
+
+conversorGraus ();
+
+function conversorGraus () {
+    
+    let celcius;
+    let fahrenheit;
+    let temperatura = prompt (Number ('Digite uma temperatura (Apenas números):'));
+    let tipoTemperatura = prompt ('É em Celcius ou Fahrenheit? Digite 1 para Celcius e 2 para Fahrenheit.');
+
+    while (tipoTemperatura !== 1 || tipoTemperatura !== 2) {
+    tipoTemperatura = prompt ('É em Celcius ou Fahrenheit? Digite 1 para Celcius e 2 para Fahrenheit.');
+    }
+
+    if (tipoTemperatura == 1) {
+    conversorGrausCparaF (temperatura);
+    } else {
+    conversorGrausFparaC (temperatura);
+    }
+
+}
+
+function conversorGrausCparaF (temp) {
+    celcius = temp;
+    fahrenheit = celcius * 9 / 5;
+    console.log (`${fahrenheit}°F`);
+}
+
+function conversorGrausFparaC (temp) {
+    fahrenheit = temp;
+    celcius = fahrenheit * 5 / 9;
+    console.log (`${celcius}°C`);
+}
